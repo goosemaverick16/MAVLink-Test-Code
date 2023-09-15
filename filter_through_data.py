d0aacf1402a043017e625475466482b67d97c9fc
goosemaverick16 <nickhoward1660@gmail.com>
2023-09-15 14:16:41 -0500

    committed this late, but this is what was used on the 9/13 Eaglet III Test

--- filter_through_data.py
+++ filter_through_data.py
@@ -20,20 +20,15 @@ def update_time():
     date = date.replace('09:', '9:')
     return date
 
-def open_file_and_write():
-    print("writing")
-
-def open_file_and_append():
-    print("Appending")
-
 if __name__ == "__main__": 
     
     try:
+        
         connection = mavutil.mavlink_connection('udpin:192.168.4.2:5760')
         connection.mav.request_data_stream_send(connection.target_system, connection.target_component, mavutil.mavlink.MAV_DATA_STREAM_ALL, 5 ,1)
         
         
-        if os.path.getsize("TelemetryData.txt") > 0:
+        '''if os.path.getsize("TelemetryData.txt") > 0:
             window = tk.Tk()
             warning = tk.Label(text = "Warning! Telemetry exists in the file. Rewrite file?", font = ("Roboto",24))
             yes_button = tk.Button(window, text = "Yes", font = ("Roboto",24), command = lambda: [open_file_and_write(), window.destroy()])
@@ -41,53 +36,59 @@ if __name__ == "__main__":
             warning.pack()
             yes_button.pack()
             no_button.pack()
-            window.mainloop()
-
-        else:
-        #file = open("TelemetryData.txt","w")
-            pass
-
+            window.mainloop()'''
+     
+        file = open("TelemetryData.txt", "a")
+        file.write("----------------------------------------------------------------------------------\n")
 
         file.write("Date and Time: " + update_time() + "\n")
-        file.write("Hello")
-        file.close()
+        file.write("\n\n")
 
         #Wait for a heartbeat!
-        #connection.wait_heartbeat()
-        #print("Heartbeat from system (system %u, component %u)" %
-        #    (connection.target_system,connection.target_component))
+        connection.wait_heartbeat()
+        print("Heartbeat from system (system %u, component %u)" %
+            (connection.target_system,connection.target_component))
 
         while True:
+            file = open("TelemetryData.txt", "a")
             #Lots of data will get printed beware!
             #To parse certain aspects of telemetry, you have to specify the type, which will be seen in all caps.
             #That is, if you print all the telemetry. 
-            '''msg = connection.recv_match(type = 'ATTITUDE',blocking=True)
+            msg = connection.recv_match(type = 'ATTITUDE',blocking=True)
             print(msg)
-            file.write(msg +"\n")
+            file.write(str(msg) +"\n")
 
             print("----------------------------------------------------------------------------------")
+            file.write("----------------------------------------------------------------------------------\n")
 
             msg = connection.recv_match(type = 'VFR_HUD',blocking=True)
             print(msg)
-            file.write(msg +"\n")
+            file.write(str(msg) +"\n")
 
             print("----------------------------------------------------------------------------------")
+            file.write("----------------------------------------------------------------------------------\n")
+
 
             msg = connection.recv_match(type = 'BATTERY_STATUS',blocking=True)
             print(msg)
-            file.write(msg +"\n")
+            file.write(str(msg) +"\n")
 
             print("----------------------------------------------------------------------------------")
+            file.write("----------------------------------------------------------------------------------\n")
+
 
             msg = connection.recv_match(type = 'GLOBAL_POSITION_INT',blocking=True)
             print(msg)
-            file.write(msg +"\n")
+            file.write(str(msg) +"\n")
 
         
             print("----------------------------------------------------------------------------------")
-            '''
+            file.write("----------------------------------------------------------------------------------\n")
 
-        file.close()
+            file.close()
+            
+
+        
 
     except OSError: #Handles no Address Error
     
